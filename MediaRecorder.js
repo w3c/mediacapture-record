682ef9675ce390ba7e8f60a761cc09ed1408be70
JimBarnett <jim@haynes-barnett.net>
2014-12-04 13:53:41 -0500

    add release script

---
+++ MediaRecorder.js
@@ -0,0 +1,23 @@
+           var respecConfig = {
+                  specStatus: "ED",
+                  editors: [{
+                        name: "Jim Barnett",
+                        company: "Genesys",
+},
+                     { name: "Travis Leithead",
+                            company: "Microsoft Corp.",
+                            url: "mailto:travis.leithead@microsoft.com?subject=MediaStream Capture Scenarios Feedback",
+                            companyURL: "http://www.microsoft.com"}],
+                  // publishDate:  "2013-11-08",
+                  edDraftURI:   "http://dvcs.w3.org/hg/dap/raw-file/tip/media-stream-capture/RecordingProposal.html",
+                  previousPublishDate: null,
+                  prevED: "https://dvcs.w3.org/hg/dap/raw-file/default/media-stream-capture/MediaRecorder.html",
+                  noIDLIn:  true,
+                  inlineCSS:  true,
+                  noRecTrack:  true,
+                  shortName:  "mediastream-recording",
+                  wg:         ["Device APIs Working Group", "Web Real-Time Communications Working Group"],
+                  wgURI:    ["http://www.w3.org/2009/dap/", "http://www.w3.org/2011/04/webrtc/"],
+                  wgPublicList: "public-media-capture",
+                  wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/43696/status", "http://www.w3.org/2004/01/pp-impl/47318/status"]
+              };
