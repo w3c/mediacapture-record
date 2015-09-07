w3c/mediacapture-record

commit 87e1361c1b20b9237899f54d512f1ba037eb7589

Add w3cid for the editors

MediaRecorder.js
@@ -3,11 +3,13 @@
                   editors: [{
                         name: "Jim Barnett",
                         company: "Genesys",
+                        w3cid: 34604
 },
                      { name: "Travis Leithead",
                             company: "Microsoft Corp.",
                             url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
-                            companyURL: "http://www.microsoft.com"}],
+                            companyURL: "http://www.microsoft.com"
+                            w3cid: 40117}],
                   // publishDate:  "2013-11-08",
                   edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
                   previousPublishDate: null,

commit 5fcd032be9306d251c3c2630202e1f0565d0b3e0

fix JSON due to a bug introduced in previous commit 87e1361

MediaRecorder.js
@@ -8,7 +8,7 @@
                      { name: "Travis Leithead",
                             company: "Microsoft Corp.",
                             url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
-                            companyURL: "http://www.microsoft.com"
+                            companyURL: "http://www.microsoft.com",
                             w3cid: 40117}],
                   // publishDate:  "2013-11-08",
                   edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
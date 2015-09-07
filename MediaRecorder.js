5fcd032be9306d251c3c2630202e1f0565d0b3e0
Vivien Lacourba <vivien@w3.org>
2015-09-07 17:44:28 +0200

    fix JSON due to a bug introduced in previous commit 87e1361

--- MediaRecorder.js
+++ MediaRecorder.js
@@ -8,7 +8,7 @@
                      { name: "Travis Leithead",
                             company: "Microsoft Corp.",
                             url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
-                            companyURL: "http://www.microsoft.com"
+                            companyURL: "http://www.microsoft.com",
                             w3cid: 40117}],
                   // publishDate:  "2013-11-08",
                   edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
