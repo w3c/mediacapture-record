d8da8a03f4789a56259a13b59f17adf7f1275e8e
Miguel Casas-Sanchez <mcasas@chromium.org>
2015-12-04 15:15:49 -0800

    Removed [TreatUndefinedAs=Missing] from MR idl. Corrected some js/html indents.

--- MediaRecorder.js
+++ MediaRecorder.js
@@ -1,26 +1,27 @@
-           var respecConfig = {
-                  specStatus: "ED",
-                  editors: [{
-                        name: "Jim Barnett",
-                        company: "Genesys",
-                        w3cid: 34604
-},
-                     { name: "Travis Leithead",
-                            company: "Microsoft Corp.",
-                            url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
-                            companyURL: "http://www.microsoft.com",
-                            w3cid: 40117}],
-                  // publishDate:  "2013-11-08",
-                  edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
-                  previousPublishDate: null,
-                  prevED: "http://w3c.github.io/mediacapture-record/archives/20150603/MediaRecorder.html",
-                  noIDLIn:  true,
-                  inlineCSS:  true,
-                  noRecTrack:  false,
-                  copyrightStart: 2012,
-                  shortName:  "mediastream-recording",
-                  wg:         ["Device APIs Working Group", "Web Real-Time Communications Working Group"],
-                  wgURI:    ["http://www.w3.org/2009/dap/", "http://www.w3.org/2011/04/webrtc/"],
-                  wgPublicList: "public-media-capture",
-                  wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/43696/status", "http://www.w3.org/2004/01/pp-impl/47318/status"]
-              };
+var respecConfig = {
+    specStatus: "ED",
+    editors: [{
+        name: "Jim Barnett",
+        company: "Genesys",
+        w3cid: 34604
+    }, { 
+        name: "Travis Leithead",
+        company: "Microsoft Corp.",
+        url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
+        companyURL: "http://www.microsoft.com",
+        w3cid: 40117
+    }],
+    // publishDate:  "2013-11-08",
+    edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
+    previousPublishDate: null,
+    prevED: "http://w3c.github.io/mediacapture-record/archives/20150603/MediaRecorder.html",
+    noIDLIn:  true,
+    inlineCSS:  true,
+    noRecTrack:  false,
+    copyrightStart: 2012,
+    shortName:  "mediastream-recording",
+    wg:         ["Device APIs Working Group", "Web Real-Time Communications Working Group"],
+    wgURI:    ["http://www.w3.org/2009/dap/", "http://www.w3.org/2011/04/webrtc/"],
+    wgPublicList: "public-media-capture",
+    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/43696/status", "http://www.w3.org/2004/01/pp-impl/47318/status"]
+};
