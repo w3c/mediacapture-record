ab009d448a7770bc431e02d2e0e1cf07b6eb81dd
Miguel Casas-Sanchez <mcasas@chromium.org>
2016-07-26 19:21:38 -0700

    rebased gh-pages (yeah?)

--- MediaRecorder.js
+++ MediaRecorder.js
@@ -1,26 +1,35 @@
-           var respecConfig = {
-                  specStatus: "ED",
-                  editors: [{
-                        name: "Jim Barnett",
-                        company: "Genesys",
-                        w3cid: 34604
-},
-                     { name: "Travis Leithead",
-                            company: "Microsoft Corp.",
-                            url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
-                            companyURL: "http://www.microsoft.com",
-                            w3cid: 40117}],
-                  // publishDate:  "2013-11-08",
-                  edDraftURI:   "http://w3c.github.io/mediacapture-record/MediaRecorder.html",
-                  previousPublishDate: null,
-                  prevED: "http://w3c.github.io/mediacapture-record/archives/20150603/MediaRecorder.html",
-                  noIDLIn:  true,
-                  inlineCSS:  true,
-                  noRecTrack:  false,
-                  copyrightStart: 2012,
-                  shortName:  "mediastream-recording",
-                  wg:         ["Device APIs Working Group", "Web Real-Time Communications Working Group"],
-                  wgURI:    ["http://www.w3.org/2009/dap/", "http://www.w3.org/2011/04/webrtc/"],
-                  wgPublicList: "public-media-capture",
-                  wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/43696/status", "http://www.w3.org/2004/01/pp-impl/47318/status"]
-              };
+var respecConfig = {
+    specStatus: "ED",
+    editors: [{
+        name: "Miguel Casas-Sanchez",
+        company: "Google, Inc.",
+        companyURL: "http://www.google.com",
+        url: "mailto:mcasas@chromium.org?subject=MediaStream%20Recording%20W3C%20Spec",
+        w3cid: 82825
+    }, {
+        name: "Jim Barnett",
+        company: "Genesys",
+        w3cid: 34604,
+        note: "Former editor"
+    }, { 
+        name: "Travis Leithead",
+        company: "Microsoft Corp.",
+        url: "mailto:travis.leithead@microsoft.com?subject=MediaStream%20Capture%20Scenarios%20Feedback",
+        companyURL: "http://www.microsoft.com",
+        w3cid: 40117,
+        note: "Former editor"
+    }],
+    // publishDate:  "2013-11-08",
+    edDraftURI:   "https://w3c.github.io/mediacapture-record/MediaRecorder.html",
+    previousPublishDate: null,
+    prevED: "https://w3c.github.io/mediacapture-record/archives/20160120/MediaRecorder.html",
+    noIDLIn:  true,
+    inlineCSS:  true,
+    noRecTrack:  false,
+    copyrightStart: 2012,
+    shortName:  "mediastream-recording",
+    wg:         ["Device and Sensors Working Group", "Web Real-Time Communications Working Group"],
+    wgURI:    ["http://www.w3.org/2009/dap/", "http://www.w3.org/2011/04/webrtc/"],
+    wgPublicList: "public-media-capture",
+    wgPatentURI:  ["https://www.w3.org/2004/01/pp-impl/43696/status", "https://www.w3.org/2004/01/pp-impl/47318/status"]
+};
